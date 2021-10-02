47f9cf5a7cea8d959070b4ee49d731054770b2d0
xoselewis <josel.martinezp88@gmail.com>
2021-10-02 01:05:59 -0300

    Começando desafio...

---
+++ src/main/java/br/com/sicredidesafio/SicredidesafioApplication.java
@@ -0,0 +1,13 @@
+package br.com.sicredidesafio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SicredidesafioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SicredidesafioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/sicredidesafio/SicredidesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sicredidesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicredidesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
