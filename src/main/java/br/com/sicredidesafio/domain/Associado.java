ab064e4c1576c4fa4b734b56b5e51bba638c39a0
xoselewis <josel.martinezp88@gmail.com>
2021-10-03 02:59:31 -0300

    Adiciona campo nome ao associado...

--- src/main/java/br/com/sicredidesafio/domain/Associado.java
+++ src/main/java/br/com/sicredidesafio/domain/Associado.java
@@ -5,6 +5,8 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.br.CPF;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Set;
 
 @Entity
@@ -13,8 +15,15 @@ import java.util.Set;
 @Table(name = "associado")
 public class Associado extends Entidad {
 
+    @Column(nullable = false)
+    @NotBlank
     @CPF(message="cpf inválido")
     String cpf;
+
+    @Column(nullable = false)
+    @NotBlank
+    @Size(max = 50)
+    String nome;
 
     @OneToMany(mappedBy = "associado", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     private Set<Votacao> votos;
